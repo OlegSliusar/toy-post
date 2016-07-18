d63afc8d43179feb5172f2170ea9da026e9382b9
Oleh Sliusar <olehsliusar@gmail.com>
2016-07-18 07:42:57 +0300

    Modify views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates :name, presence: true    # Replace FILL_IN with the right code.
-  validates :email, presence: true    # Replace FILL_IN with the right code.
+  validates :name, length: { maximum: 50 },
+                              presence: true    # Replace FILL_IN with the right code.
+  validates :email, length: { maximum: 50 },
+                              presence: true    # Replace FILL_IN with the right code.
 end
