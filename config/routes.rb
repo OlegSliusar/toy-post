646fc70a87eb4dcaeeb24e3caffa02e04a408462
Oleh Sliusar <olehsliusar@gmail.com>
2016-08-22 13:39:35 +0300

    Filling layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
-  root 'microposts#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'microposts#new'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
