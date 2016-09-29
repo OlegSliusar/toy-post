7e347ef7a9935cfa067c585555d3c4a6f529f519
Oleh Sliusar <oleh@olehsliusar.com>
2016-09-29 10:30:48 +0300

    Implement advanced login

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,34 +1,25 @@
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: [:show, :edit, :update, :destroy]
 
-  # GET /microposts
-  # GET /microposts.json
   def index
     @microposts = Micropost.all
   end
 
-  # GET /microposts/1
-  # GET /microposts/1.json
   def show
     @users = User.all
     @user = User.find(@micropost.user_id)
-    #console
   end
 
-  # GET /microposts/new
   def new
     @microposts = Micropost.all
     @micropost = Micropost.new
   end
 
-  # GET /microposts/1/edit
   def edit
   end
 
-  # POST /microposts
-  # POST /microposts.json
   def create
-    @micropost = Micropost.new(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
 
     respond_to do |format|
       if @micropost.save
@@ -44,8 +35,6 @@ class MicropostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /microposts/1
-  # PATCH/PUT /microposts/1.json
   def update
     respond_to do |format|
       if @micropost.update(micropost_params)
@@ -61,8 +50,6 @@ class MicropostsController < ApplicationController
     end
   end
 
-  # DELETE /microposts/1
-  # DELETE /microposts/1.json
   def destroy
     @micropost.destroy
     respond_to do |format|
@@ -73,6 +60,7 @@ class MicropostsController < ApplicationController
   end
 
   private
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_micropost
       @micropost = Micropost.find(params[:id])
@@ -80,6 +68,6 @@ class MicropostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
-      params.require(:micropost).permit(:content, :user_id)
+      params.require(:micropost).permit(:content)
     end
 end
