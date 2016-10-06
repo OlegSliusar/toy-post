3c35ef0c6daaf0757505aa875e322e7999892abb
Oleh Sliusar <oleh@olehsliusar.com>
2016-10-06 14:15:42 +0300

    Prepare email for production

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,17 +1,6 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # def setup
-  #   @user = users(:oleh)
-  # end
-
-  # test "password_reset" do
-  #   mail = UserMailer.password_reset(@user)
-  #   assert_equal "Password reset", mail.subject
-  #   assert_equal [@user.email], mail.to
-  #   assert_equal ["noreply@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
 
   test "password_reset" do
     user = users(:oleh)
@@ -19,7 +8,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset",        mail.subject
     assert_equal [user.email],            mail.to
-    assert_equal ["noreply@example.com"], mail.from
+    assert_equal ["noreply@toypost.com"], mail.from
     assert_match user.reset_token,        mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
